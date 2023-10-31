d49aa2c1f850f4313064e2b1effd99622e275725
Earth . <arvenny123@gmail.com>
2023-10-31 17:50:10 +0700

    native map-navigation

--- App.js
+++ App.js
@@ -1,73 +1,25 @@
-import React from "react";
-import MapView, { Callout, Marker } from "react-native-maps";
-import { StyleSheet, View, Text, Image } from "react-native";
+// In App.js in a new project
 
-const data = [
-  {
-    lat: 13.7563,
-    lon: 100.5018,
-    tex: "PTT01",
-  },
-  {
-    lat: 13.7503,
-    lon: 100.525,
-    tex: "OOP02",
-  },
-  {
-    lat: 13.7553,
-    lon: 100.52,
-    tex: "OOP03",
-  },
-];
+import * as React from 'react';
+import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import LoginScreen from './screen/LoginScreen';
+import MapScreen from './screen/MapScreen';
 
-export default function App() {
+
+
+const Stack = createNativeStackNavigator();
+
+function App() {
   return (
-    <View style={styles.container}>
-      <MapView
-        style={styles.map}
-        initialRegion={{
-          latitude: 13.7563,
-          longitude: 100.5018,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }}
-      >
-        {data.map((item, index) => (
-          <Marker
-            key={index}
-            coordinate={{ latitude: item.lat, longitude: item.lon }}
-          >
-            <Callout tooltip>
-              <View style={styles.bubble}>
-                <Text>{item.tex}</Text>
-              </View>
-              <Image
-                source={require("./assets/img/toilet.jpg")}
-                style={{ width: 150, height: 150, zIndex: 1 }}
-              />
-            </Callout>
-          </Marker>
-        ))}
-      </MapView>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Login' >
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Map" component={MapScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  map: {
-    width: "100%",
-    height: "100%",
-  },
-  bubble: {
-    flexDirection: "row",
-    alignSelf: "flex-start",
-    backgroundColor: "#fff",
-    borderRadius: 6,
-    borderColor: "#ccc",
-    borderWidth: 16,
-    alignSelf: "center",
-  },
-});
+export default App;
---
+++ screen/MapScreen.js
@@ -0,0 +1,73 @@
+import React from "react";
+import MapView, { Callout, Marker } from "react-native-maps";
+import { StyleSheet, View, Text, Image } from "react-native";
+
+const data = [
+  {
+    lat: 13.7563,
+    lon: 100.5018,
+    tex: "PTT01",
+  },
+  {
+    lat: 13.7503,
+    lon: 100.525,
+    tex: "OOP02",
+  },
+  {
+    lat: 13.7553,
+    lon: 100.52,
+    tex: "OOP03",
+  },
+];
+
+export default function MapScreen() {
+  return (
+    <View style={styles.container}>
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          latitude: 13.7563,
+          longitude: 100.5018,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      >
+        {data.map((item, index) => (
+          <Marker
+            key={index}
+            coordinate={{ latitude: item.lat, longitude: item.lon }}
+          >
+            <Callout>
+              <View >
+                <Text>{item.tex}</Text>
+              </View>
+              <Image
+                source={require("../assets/img/toilet.jpg")}
+                style={{ width: 150, height: 150, zIndex: 1 }}
+              />
+            </Callout>
+          </Marker>
+        ))}
+      </MapView>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  map: {
+    width: "100%",
+    height: "100%",
+  },
+  // bubble: {
+  //   flexDirection: "row",
+  //   alignSelf: "flex-start",
+  //   backgroundColor: "#fff",
+  //   borderRadius: 6,
+  //   borderColor: "#ccc",
+  //   borderWidth: 16,
+  //   alignSelf: "center",
+  // }, 
+});
